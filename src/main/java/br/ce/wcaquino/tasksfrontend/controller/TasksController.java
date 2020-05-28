bc2d8a9d6f1fd9c69d48864d7cd380148b0b2896
Wesley <wesley.goncalves@furukawaelectric.com>
2020-05-27 22:44:34 -0300

    fix sucess

--- src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/controller/TasksController.java
@@ -16,20 +16,20 @@ import br.ce.wcaquino.tasksfrontend.model.Todo;
 
 @Controller
 public class TasksController {
-	
+
 	@Value("${backend.host}")
 	private String BACKEND_HOST;
 
 	@Value("${backend.port}")
 	private String BACKEND_PORT;
-	
+
 	@Value("${app.version}")
 	private String VERSION;
-	
+
 	public String getBackendURL() {
 		return "http://" + BACKEND_HOST + ":" + BACKEND_PORT;
 	}
-	
+
 	@GetMapping("")
 	public String index(Model model) {
 		model.addAttribute("todos", getTodos());
@@ -37,7 +37,7 @@ public class TasksController {
 			model.addAttribute("version", VERSION);
 		return "index";
 	}
-	
+
 	@GetMapping("add")
 	public String add(Model model) {
 		model.addAttribute("todo", new Todo());
@@ -49,8 +49,8 @@ public class TasksController {
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			restTemplate.postForObject(
-					getBackendURL() + "/tasks-backend/todo", todo, Object.class);			
-			model.addAttribute("sucess", "Sucess!");
+					getBackendURL() + "/tasks-backend/todo", todo, Object.class);
+			model.addAttribute("success", "Success!");
 			return "index";
 		} catch(Exception e) {
 			Pattern compile = Pattern.compile("message\":\"(.*)\",");
@@ -58,22 +58,22 @@ public class TasksController {
 			m.find();
 			model.addAttribute("error", m.group(1));
 			model.addAttribute("todo", todo);
-			return "add"; 
+			return "add";
 		} finally {
 			model.addAttribute("todos", getTodos());
 		}
 	}
-	
+
 	@GetMapping("delete/{id}")
 	public String delete(@PathVariable Long id, Model model) {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.delete(getBackendURL() + "/tasks-backend/todo/" + id);			
+		restTemplate.delete(getBackendURL() + "/tasks-backend/todo/" + id);
 		model.addAttribute("success", "Success!");
 		model.addAttribute("todos", getTodos());
 		return "index";
 	}
 
-	
+
 	@SuppressWarnings("unchecked")
 	private List<Todo> getTodos() {
 		RestTemplate restTemplate = new RestTemplate();
